25f6aadc509802581bbb84d82f2b4ce1977b5086
MarinaCouso <marina.couso.martinez@gmail.com>
2022-02-27 14:10:28 +0100

    style: improve atoms styles

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,42 +1,39 @@
-import React from 'react';
-import { Pressable, StyleSheet, TextStyle, ViewStyle } from 'react-native';
-import defaultTheme from '../../theme/theme';
-import { Text } from './text';
+import React from "react";
+import { Pressable, StyleSheet, TextStyle, ViewStyle } from "react-native";
+import defaultTheme from "../../theme/theme";
+import { Text } from "./text";
 
 type Props = {
   containerStyle?: ViewStyle;
   textStyle?: TextStyle;
   title: string;
   onPress: () => void;
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: defaultTheme.$primaryColor,
-    width: '40%',
+    width: "40%",
     padding: 5,
     borderRadius: 7,
   },
   text: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 17,
     color: defaultTheme.$baseColor,
-  }
+  },
 });
 
-const Button = ({
-  containerStyle,
-  textStyle,
-  title,
-  onPress,
-}: Props) => (
-  <Pressable style={styles.container} onPress={onPress}>
-    <Text text={title} style={styles.text}/>
+const Button = ({ containerStyle, textStyle, title, onPress }: Props) => (
+  <Pressable style={containerStyle} onPress={onPress}>
+    <Text text={title} style={textStyle} />
   </Pressable>
 );
-  
+
 Button.defaultProps = {
-  text: 'Button',
+  containerStyle: styles.container,
+  textStyle: styles.text,
+  text: "Button",
 };
 
 export { Button };
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,6 +2,7 @@ const defaultTheme = {
   $backgroundColor: "#AFE773",
   $primaryColor: "#4634DA",
   $baseColor: "#FFFFFF",
+  $secondaryColor: "#79726F",
 };
 
 export default defaultTheme;
