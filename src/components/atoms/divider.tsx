3618507369885666ab266e4697b84e36bb71119e
MarinaCouso <marina.couso.martinez@gmail.com>
2022-02-27 14:08:11 +0100

    feat: create divider component

---
+++ src/components/atoms/divider.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, ViewStyle, StyleSheet } from "react-native";
+import defaultTheme from "../../theme/theme";
+
+type Props = {
+  style?: ViewStyle;
+  transparent?: boolean;
+};
+
+const styles = StyleSheet.create({
+  dividerStyle: {
+    marginTop: "7%",
+    marginBottom: "7%",
+    width: "30%",
+    alignSelf: "center",
+  },
+  boderStyle: {
+    borderBottomWidth: 1,
+    borderBottomColor: defaultTheme.$secondaryColor,
+  },
+});
+
+const Divider = ({ style, transparent }: Props) => {
+  const appliedStyles = StyleSheet.flatten([
+    style ? style : styles.dividerStyle,
+    transparent ? null : styles.boderStyle,
+  ]);
+  return <View style={appliedStyles} />;
+};
+
+Divider.defaultProps = {
+  transparent: false,
+};
+
+export { Divider };
