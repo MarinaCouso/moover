2b7651ebd557afe5740bb5f1d906f1c2a9c82376
MarinaCouso <marina.couso.martinez@gmail.com>
2022-02-27 19:19:01 +0100

    feat: add fetch movies from api functionality

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { Divider } from "./src/components/atoms/divider";
 import { Card } from "./src/components/molecules/card";
 import { Header } from "./src/components/molecules/header";
 import { SearchForm } from "./src/components/molecules/search-section";
 import defaultTheme from "./src/theme/theme";
+import { getMoviesFromApi } from "./src/api/api";
 
 const styles = StyleSheet.create({
   appContainer: {
@@ -15,6 +16,19 @@ const styles = StyleSheet.create({
 
 export default function App() {
   const [inputValue, setInputValue] = useState<string>("");
+  const [isLoading, setLoading] = useState(true);
+  const [moviesList, setMoviesList] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const movies = await getMoviesFromApi(inputValue);
+      setMoviesList(movies);
+    } catch (error) {
+      console.error(error, "error");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.appContainer}>
@@ -24,7 +38,7 @@ export default function App() {
       <SearchForm
         setInputValue={setInputValue}
         inputValue={inputValue}
-        onSearch={() => console.log(inputValue)}
+        onSearch={getMovies}
       />
       <Divider />
       <Card />
---
+++ src/api/api.tsx
@@ -0,0 +1,13 @@
+import { API_KEY } from "./constants";
+
+export const getMoviesFromApi = async (query: string) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
+    );
+    const json = await response.json();
+    return json.results;
+  } catch (error) {
+    console.error(error);
+  }
+};
