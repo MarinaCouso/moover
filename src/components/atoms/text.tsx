c607744b0e57c7c1a9d1f7931e3fcad4f65ad64a
MarinaCouso <marina.couso.martinez@gmail.com>
2022-02-27 13:20:33 +0100

    feat: create text component

---
+++ src/components/atoms/text.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text as TextNativeComponent, TextStyle } from 'react-native';
+import defaultTheme from '../../theme/theme';
+
+type Props = {
+  style?: TextStyle;
+  text: string;
+}
+
+const Text = ({
+  style,
+  text,
+}: Props) => (
+  <TextNativeComponent style={style}>{text}</TextNativeComponent>
+  );
+
+Text.defaultProps = {
+  style: {
+    fontSize: 30,
+    padding: 5,
+    color: defaultTheme.$primaryColor,
+  },
+};
+
+export { Text };
